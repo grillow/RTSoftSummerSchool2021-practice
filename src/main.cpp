0ff8c8be6a5b6429bd41c41dc66b2e4ba041def0
grillow <spmr757@gmail.com>
2021-07-19 17:57:43 +0300

    more skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,27 @@
 #include "LineDetector.hpp"
 
 
+double distance_to_dash_line(const cv::Mat & frame) {
+    
+    const auto line = LineDetector::DetectDashLine(frame);
+    const double distance = LineDetector::GetDistanceToLine(line);
+    
+    return distance;
+}
+
+
 auto main() -> int {
 
     CarController controller;
     controller.SetMovingStrategy(std::make_unique<PrintingMovingStrategy>(std::cout));
 
     // get a frame somehow
+    cv::VideoCapture cap(0);
     cv::Mat frame;
-    
-    // for this frame:
-    const auto line = LineDetector::DetectDashLine(frame);
-    const double distance = LineDetector::GetDistanceToLine(line);
-    controller.Move(distance);
+    while (cap.read(frame)) {
+        const double distance = distance_to_dash_line(frame);
+        controller.Move(distance);
+    }
 
     return 0;
 }
